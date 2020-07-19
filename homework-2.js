41309cb142fbdef7e543cf93a6207d367c8381ee
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-07-19 13:54:49 +0300

    homework 02

---
+++ homework-2.js
@@ -0,0 +1,13 @@
+const N_FIRST = +prompt("Enter a number");
+const M_SECOND = +prompt("Enter the number again");
+
+if (N_FIRST && M_SECOND === Number.isInteger(N_FIRST && M_SECOND)) {
+  const SKIP_NUMBERS = confirm('Skip even numbers?');
+  let numbers = 0;
+  for (let i = N_FIRST; i <= M_SECOND; i++); {
+    if (SKIP_NUMBERS && i % 2 === 0) continue;
+    else {
+      numbers += i;
+    }
+  }
+}
