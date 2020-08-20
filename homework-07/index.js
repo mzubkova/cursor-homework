fe6d4d04aee6b0beb1b9151e76933ae8c006ba01
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-08-20 15:34:45 +0300

    corrected mistakes

--- homework-07/index.js
+++ homework-07/index.js
@@ -5,33 +5,33 @@ const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 // № 1 Створіть функцію getMyTaxes.call(country, salary) -> number; – яка рахує скільки податків ви заплатите як IT - спеціаліст в якійсь з країн.Функція повинна викликатись через call та працювати з даними через this
 
 function getMyTaxes(salary) {
-  return Math.round(this.tax * salary);
+  return this.tax * salary;
 }
 
 // № 2 Створіть функцію getMiddleTaxes.call(country) -> number; – яка рахує скільки у середньому податків платятт IT - спеціалісти у кожній країні. (tax * middleSalary).Функція повинна викликатись через call та працювати з даними через this
 
 function getMiddleTaxes() {
-  return Math.round(this.tax * this.middleSalary);
+  return this.tax * this.middleSalary;
 }
 
 // № 3 Створіть функцію getTotalTaxes.call(country) -> number; – яка рахує, скільки всього податків платять IT - спеціалісти у кожній країні. (tax * middleSalary * vacancies).Функція повинна викликатись через call та працювати з даними через this
 
 function getTotalTaxes() {
-  return Math.round(this.tax * this.middleSalary * this.vacancies);
+  return this.tax * this.middleSalary * this.vacancies;
 }
 
 // № 4 Створіть функцію getMySalary(country) – яка буде писати в консоль об'єкт виду: { salary: number, taxes: number, profit: number } кожні 10 секунд. Значення salary – генеруйте випадковим чином у діапазоні 1500-2000. taxes – розраховується в залежності від вибраної країни та значення salary.
 // profit = salary - taxes;
 
 function getMySalary(country) {
-  let minSalary = 1500;
-  let maxSalary = 2000;
+  const minSalary = 1500;
+  const maxSalary = 2000;
   setInterval(function () {
     let salary = Math.floor(
       Math.random() * (maxSalary - minSalary) + minSalary
     );
-    let taxes = Math.round(country.tax * salary);
-    let profit = Math.round(salary - taxes);
+    const taxes = country.tax * salary;
+    const profit = salary - taxes;
     return console.log({
       salary: salary,
       taxes: taxes,
