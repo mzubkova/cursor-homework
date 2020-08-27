fc5a1347a229e48f086ff237401ad11bf22c3e2c
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-08-27 15:07:44 +0300

    homework 10

---
+++ homework-10/index.js
@@ -0,0 +1,31 @@
+function playSound(e) {
+  const attribute = `[data-key="${event.keyCode}"]`;
+  const audio = document.querySelector('audio' + attribute);
+  const key = document.querySelector('div' + attribute);
+  if (event.keyCode === key) audio.currentTime = 0;
+  audio.play();
+}
+
+document.addEventListener('keydown', playSound);
+
+// function playSound(e) {
+//   const attribute = `[data-key="${e.keyCode}"]`;
+//   const audio = document.querySelector('audio' + attribute);
+//   const key = document.querySelector('div' + attribute);
+
+//   if (!key)
+//     return;
+
+//   key.classList.add('playing');
+//   audio.currentTime = 0;
+//   audio.play();
+// };
+
+// function removeTransition(e) {
+//   if (e.propertyName !== 'transform') return;
+//   this.classList.remove('playing');
+// };
+
+// const keys = Array.from(document.querySelectorAll('.key'));
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+// document.addEventListener('keydown', playSound);
